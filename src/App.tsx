bbe2c9101c8f0d4e67dea99ce209090cd16a94fe
Bruno Mariz <bruno.mariz@semantix.inc>
2022-03-01 02:48:23 -0300

    refac: change browse to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./routes/Home";
 import Analytics from "./routes/Analytics";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/analytics" element={<Analytics></Analytics>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
