893abf206d1fb057ccbe5825839f046dbe880775
Bruno Mariz <bruno.mariz@semantix.inc>
2022-03-12 11:22:08 -0300

    revert back to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Analytics from "./routes/Analytics/Analytics";
 import Home from "./routes/Home";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/analytics" element={<Analytics></Analytics>}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
